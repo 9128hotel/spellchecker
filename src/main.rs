6335a8e15d8e6510f2e52b7a280d5ee6ecd260b9
Lucy B <lucybeyley@gmail.com>
2022-09-09 18:03:30 +0100

    modified:   src/main.rs 	modified:   src/micro.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{fs};
 fn main() {
     let file_path="C:\\Users\\octon\\Documents\\git-cloned\\spellchecker\\src\\words.txt";
+    //start
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut dict: Vec<&str>=Vec::new();
     for byte in contents.split_whitespace() {
--- src/micro.rs
+++ src/micro.rs
@@ -1,3 +1,12 @@
-pub fn similarity(input1:String, dict:Vec<String>) {
-    let input1_vec: Vec<char> = input1.chars().collect();
+use std::{fs};
+
+pub fn spellcheck(input1:String, dict:Vec<String>) {
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let mut dict: Vec<&str>=Vec::new();
+    for byte in contents.split_whitespace() {
+        dict.push(byte);
+    }
+    for x in &dict {
+        println!("{x}");
+    }
 }
