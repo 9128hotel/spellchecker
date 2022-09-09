f80bcda5be894b4d9b5620ccd2bed8ec3719f358
Lucy B <lucybeyley@gmail.com>
2022-09-09 19:27:01 +0100

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use std::{fs};
+use std::{fs, io::{stdin}};
 fn main() {
     let file_path="C:\\Users\\octon\\Documents\\git-cloned\\spellchecker\\src\\words.txt";
+    let mut check = String::new();
+    stdin().read_line(&mut check).unwrap();
+
     //start
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents=fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut dict: Vec<&str>=Vec::new();
     for byte in contents.split_whitespace() {
         dict.push(byte);
     }
+    let index = dict.iter().position(|&r| r == "two").unwrap();
     for x in &dict {
         println!("{x}");
     }
