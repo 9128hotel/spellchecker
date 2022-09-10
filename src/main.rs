6fb37da0c99ddfab9913a99ee65c2e063c4a88a0
lucy-b3 <lucybeyley@gmail.com>
2022-09-10 01:01:30 +0100

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
-use std::{fs, io::{stdin}};
+use std::{fs, io::{stdin, stdout, Write}};
 fn main() {
-    let file_path="C:\\Users\\octon\\Documents\\git-cloned\\spellchecker\\src\\words.txt";
-    let mut check = String::new();
-    stdin().read_line(&mut check).unwrap();
-
+    let file_path="/home/lucy/code/spellchecker/src/words.txt";
+    let mut check=String::new();
+    print!("Please enter some text: ");
+    let _=stdout().flush();
+    stdin().read_line(&mut check).expect("Did not enter a correct string");
+    if let Some('\n')=check.chars().next_back() {
+        check.pop();
+    }
+    if let Some('\r')=check.chars().next_back() {
+        check.pop();
+    }
     //start
     let contents=fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut dict: Vec<&str>=Vec::new();
     for byte in contents.split_whitespace() {
         dict.push(byte);
     }
-    let index = dict.iter().position(|&r| r == "two").unwrap();
+    let index = dict.iter().position(|&a| a == check.to_owned()).unwrap(); //crashes if word doesn't exist
     for x in &dict {
         println!("{x}");
     }
+    println!("{:?}", index);
+    println!("{:?}", check);
+    println!("{:?}", dict[index]);
 }
